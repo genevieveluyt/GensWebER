3b86a0fd09a4c367e6d6da31d931ec6e6274e0c6
Genevieve Luyt <genevieve.luyt@gmail.com>
2017-04-02 15:18:38 -0700

    Update script to start mysql service and setup gensweber if it is not yet (create a virtual environment and install gensweber)

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,19 @@
 #!/bin/bash  
 
+sudo service mysql start
+
+# create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    virtualenv venv
+fi
+
+# activate virtual environment
 source venv/bin/activate
+
+# install gensweber as editable package
+pip install --editable .
+
+# start flask server
 export FLASK_APP=gensweber
 export FLASK_DEBUG=true
 PYTHONPATH=${PWD} flask run
